4ad73bf23ebaae944f641a512cc2a00a60fc8381
Mike Yim <mikeyim@unitetheculture.org>
2022-09-18 15:10:49 -0700

    Set up new profile for split keyboard

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -34,7 +34,7 @@ cmp.setup({
 		end,
 	},
 	mapping = cmp.mapping.preset.insert({
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+		["<C-y>"] = cmp.mapping.confirm({ select = true }),
 		["<C-u>"] = cmp.mapping.scroll_docs(-4),
 		["<C-d>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
@@ -90,16 +90,36 @@ local function config(_config)
 	return vim.tbl_deep_extend("force", {
 		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
 		on_attach = function()
-			nnoremap("gd", function() vim.lsp.buf.definition() end)
-			nnoremap("K", function() vim.lsp.buf.hover() end)
-			nnoremap("<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
-			nnoremap("<leader>vd", function() vim.diagnostic.open_float() end)
-			nnoremap("[d", function() vim.diagnostic.goto_next() end)
-			nnoremap("]d", function() vim.diagnostic.goto_prev() end)
-			nnoremap("<leader>vca", function() vim.lsp.buf.code_action() end)
-			nnoremap("<leader>vrr", function() vim.lsp.buf.references() end)
-			nnoremap("<leader>vrn", function() vim.lsp.buf.rename() end)
-			inoremap("<C-h>", function() vim.lsp.buf.signature_help() end)
+			nnoremap("gd", function()
+				vim.lsp.buf.definition()
+			end)
+			nnoremap("K", function()
+				vim.lsp.buf.hover()
+			end)
+			nnoremap("<leader>vws", function()
+				vim.lsp.buf.workspace_symbol()
+			end)
+			nnoremap("<leader>vd", function()
+				vim.diagnostic.open_float()
+			end)
+			nnoremap("[d", function()
+				vim.diagnostic.goto_next()
+			end)
+			nnoremap("]d", function()
+				vim.diagnostic.goto_prev()
+			end)
+			nnoremap("<leader>vca", function()
+				vim.lsp.buf.code_action()
+			end)
+			nnoremap("<leader>vrr", function()
+				vim.lsp.buf.references()
+			end)
+			nnoremap("<leader>vrn", function()
+				vim.lsp.buf.rename()
+			end)
+			inoremap("<C-h>", function()
+				vim.lsp.buf.signature_help()
+			end)
 		end,
 	}, _config or {})
 end
@@ -202,4 +222,3 @@ require("luasnip.loaders.from_vscode").lazy_load({
 	include = nil, -- Load all languages
 	exclude = { "phpdoc", "tree-sitter-php-doc" },
 })
-
