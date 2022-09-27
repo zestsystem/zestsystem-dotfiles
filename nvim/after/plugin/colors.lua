f1281d36239e8d4721ee539907a0efa48ff3087c
Mike Yim <mikeyim@unitetheculture.org>
2022-09-26 19:13:13 -0700

    now can switch between tokyonight and gruvbox BEAUTIFUL

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,43 +1,43 @@
-vim.g.zestsystem_colorscheme = "tokyonight"
+vim.g.zestsystem_colorscheme = "gruvbox"
 
 function ColorMyPencils()
-    vim.g.gruvbox_contrast_dark = 'hard'
-    vim.g.tokyonight_transparent_sidebar = true
-    vim.g.tokyonight_transparent = true
-    vim.g.gruvbox_invert_selection = '0'
-    vim.opt.background = "dark"
+	vim.g.gruvbox_contrast_dark = "hard"
+	vim.g.gruvbox_invert_selection = "0"
+	vim.opt.background = "dark"
+	if vim.g.zestsystem_colorscheme == "tokyonight" then
+		vim.g.tokyonight_transparent_sidebar = true
+		vim.g.tokyonight_transparent = true
+	end
+	vim.cmd("colorscheme " .. vim.g.zestsystem_colorscheme)
 
-    vim.cmd("colorscheme " .. vim.g.zestsystem_colorscheme)
+	local hl = function(thing, opts)
+		vim.api.nvim_set_hl(0, thing, opts)
+	end
 
-    local hl = function(thing, opts)
-        vim.api.nvim_set_hl(0, thing, opts)
-    end
+	hl("SignColumn", {
+		bg = "none",
+	})
 
-    hl("SignColumn", {
-        bg = "none",
-    })
+	hl("ColorColumn", {
+		ctermbg = 0,
+		bg = "#555555",
+	})
 
-    hl("ColorColumn", {
-        ctermbg = 0,
-        bg = "#555555",
-    })
+	hl("CursorLineNR", {
+		bg = "None",
+	})
 
-    hl("CursorLineNR", {
-        bg = "None"
-    })
+	hl("Normal", {
+		bg = "none",
+	})
+	if vim.g.zestsystem_colorscheme == "tokyonight" then
+		hl("LineNr", {
+			fg = "#5eacd3",
+		})
 
-    hl("Normal", {
-        bg = "none"
-    })
-
-    hl("LineNr", {
-        fg = "#5eacd3"
-    })
-
-    hl("netrwDir", {
-        fg = "#5eacd3"
-    })
-
+		hl("netrwDir", {
+			fg = "#5eacd3",
+		})
+	end
 end
 ColorMyPencils()
-
--- nvim/lua/zestsystem/bufferline.lua
+++ nvim/lua/zestsystem/bufferline.lua
@@ -4,7 +4,7 @@ if (not status) then return end
 bufferline.setup({
   options = {
     mode = "tabs",
-    separator_style = 'slant',
+    separator_style = 'thin',
     always_show_bufferline = true,
     show_buffer_close_icons = false,
     show_close_icon = false,
@@ -20,14 +20,14 @@ bufferline.setup({
     },
     background = {
       fg = '#657b83',
-      bg = '#002b36'
+      --bg = '#002b36'
     },
     buffer_selected = {
       fg = '#fdf6e3',
       bold = true,
     },
     fill = {
-      bg = '#073642'
+     -- bg = '#073642'
     }
   },
 })
